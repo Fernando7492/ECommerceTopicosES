14dbc30adb9ddc5d7b69f9f6f1f95ce89b2acd79
Victor <victor.hugooo@hotmail.com.br>
2024-07-23 22:11:36 -0300

    Project Creation

---
+++ price/src/test/java/com/topicos/price/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicos.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

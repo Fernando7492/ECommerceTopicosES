188f7bfb6f35a5eb73695571e1bb3c1379698400
Fernando7492 <Emidio8000@gmail.com>
2024-07-25 13:47:29 -0300

    ModelMapper added to dependencies, StandardExceptionHandler now extends ReponseEntityExceptionHandler

--- catalog/src/main/java/com/topicos/catalog/config/StandardExceptionHandler.java
+++ catalog/src/main/java/com/topicos/catalog/config/StandardExceptionHandler.java
@@ -8,12 +8,13 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class StandardExceptionHandler {
+public class StandardExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {

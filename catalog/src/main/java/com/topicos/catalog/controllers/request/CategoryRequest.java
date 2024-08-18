d254fd4c0bb647e4e875321bddd996166bcdcbfe
Fernando7492 <emidio8000@gmail.com>
2024-08-18 14:36:45 -0300

    remocao de imports nao usados

--- catalog/src/main/java/com/topicos/catalog/controllers/request/CategoryRequest.java
+++ catalog/src/main/java/com/topicos/catalog/controllers/request/CategoryRequest.java
@@ -1,13 +1,11 @@
 package com.topicos.catalog.controllers.request;
 
 import com.topicos.catalog.config.SpringApplicationContext;
-import com.topicos.catalog.create.CreateCategory;
 import com.topicos.catalog.models.Category;
 
 import lombok.Getter;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Getter @Setter

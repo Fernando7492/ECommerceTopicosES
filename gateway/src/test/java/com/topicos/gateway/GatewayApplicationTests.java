9a8467851dd93bcd2efd7109c3933123499d331d
Gabriel Viana <gabrielalmeidavn@gmail.com>
2024-08-18 21:54:24 -0300

    feat: create discovery and gateway projects

--- gateway/src/test/java/com/topicos/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/topicos/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.topicos.storage;
+package com.topicos.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StocksApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {

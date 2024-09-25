e3598477455695e5d7e74c76af085430a0022f83
Fernando7492 <emidio8000@gmail.com>
2024-09-25 16:28:11 -0300

    Autenticacao Desabilitada

--- auth/src/main/java/com/topicos/auth/ResourceServerConfig.java
+++ auth/src/main/java/com/topicos/auth/ResourceServerConfig.java
@@ -10,18 +10,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ResourceServerConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/public/**").permitAll() // Permite acesso público para qualquer rota que comece com /public
-                .anyRequest().authenticated() // Requer autenticação para todas as outras requisições
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt
-                    .jwkSetUri("http://localhost:8090/realms/tees/protocol/openid-connect/certs") // URI do conjunto de chaves JWK do Keycloak
-                )
-            );
-        return http.build();
-    }
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .authorizeHttpRequests(authorize -> authorize
+    //             .requestMatchers("/public/**").permitAll() // Permite acesso público para qualquer rota que comece com /public
+    //             .anyRequest().authenticated() // Requer autenticação para todas as outras requisições
+    //         )
+    //         .oauth2ResourceServer(oauth2 -> oauth2
+    //             .jwt(jwt -> jwt
+    //                 .jwkSetUri("http://localhost:8090/realms/tees/protocol/openid-connect/certs") // URI do conjunto de chaves JWK do Keycloak
+    //             )
+    //         );
+    //     return http.build();
+    // }
 }
